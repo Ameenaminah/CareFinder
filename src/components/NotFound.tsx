c5e494fd83d5a07c97bc6afc56f23578eb9b6d8e
Aminat Ameen <ameenaminatmorounmubo@gmail.com>
2024-03-13 23:37:19 +0100

    add

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,14 @@
+export const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "15em",
+      }}
+    >
+      <h1>NotFound</h1>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Input } from "./input/Input";
 export { Spinner } from "./Spinner";
 export { HospitalError } from "./HospitalError";
 export { ConfirmDeleteHospital } from "./ConfirmDeleteHospital";
+export { NotFound } from "./NotFound";
